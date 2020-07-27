8a755d0895d77fd0063305ac99a16e47aedb3e41
贾金达 <jiajinda@meituan.com>
2020-07-27 14:17:41 +0800

    设置好了SceneDelegate

---
+++ NotificationDemo/BViewController.h
@@ -0,0 +1,17 @@
+//
+//  BViewController.h
+//  NotificationDemo
+//
+//  Created by 贾金达 on 2020/7/27.
+//  Copyright © 2020 jiajinda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
